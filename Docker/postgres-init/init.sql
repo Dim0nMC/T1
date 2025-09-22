ada665e934f1b0817a51a3bba52e0fbdf88c444d
Dim0nMC <seliverstovd2003@gmail.com>
2025-09-23 02:39:25 +0300

    adding docker

---
+++ Docker/postgres-init/init.sql
@@ -0,0 +1,14 @@
+-- Client Processing
+CREATE DATABASE client_processing_db;
+CREATE USER client_user WITH PASSWORD '123';
+GRANT ALL PRIVILEGES ON DATABASE client_processing_db TO client_user;
+
+-- Account Processing
+CREATE DATABASE account_processing_db;
+CREATE USER account_user WITH PASSWORD '456';
+GRANT ALL PRIVILEGES ON DATABASE account_processing_db TO account_user;
+
+-- Credit Processing
+CREATE DATABASE credit_processing_db;
+CREATE USER credit_user WITH PASSWORD '789';
+GRANT ALL PRIVILEGES ON DATABASE credit_processing_db TO credit_user;
